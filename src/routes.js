78ff05a9139185ee3c39af6cf88c4ec2ccb74248
Douglas Shibata <douglas.shibata11@gmail.com>
2021-02-18 11:30:27 -0300

    adicionando controllers e rota de auth

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
 const routes = express.Router();
-const UsuarioController = require('./controllers/UsuarioController');
+const upload = multer(uploadConfig);
+
+const UsuarioController = require('./app/controllers/UsuarioController');
+const SessionController = require('./app/controllers/SessionController');
+const auth = require('./app/middlewares/auth');
 
 routes.post('/user',UsuarioController.store);
+routes.put('/user',auth,upload.single('cpfImages'),UsuarioController.update);
+routes.get('/user',auth,UsuarioController.index);
+routes.get('/perfil',auth,UsuarioController.show);
 
+routes.post('/sessions',SessionController.store);
 
 module.exports = routes;
