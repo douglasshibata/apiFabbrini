99d8ace64e6bae156482edabba0a415b6809350b
Douglas Shibata <douglas.shibata11@gmail.com>
2021-05-04 17:59:00 -0300

    Adicionando paginação

--- src/app/controllers/ResetPasswordController.js
+++ src/app/controllers/ResetPasswordController.js
@@ -1,43 +1,51 @@
+const PasswordValidator = require('password-validator');
 const User = require('../models/Usuario');
 
-var passwordValidator = require('password-validator');
-var schema = new passwordValidator();
+const schema = new PasswordValidator();
 module.exports = {
-    async store(req, res) {
-        const { tokenResetSenha, senha ,email} = req.body;
-        try {
-            const user = await User.findOne({ email }).select('+tokenResetSenha tokenResetExpiresSenha');
-            if (!user) {
-                return res.status(400).send({ error: "Usuário Não encontrado" } )
-            }
-            if (tokenResetSenha !== user.tokenResetSenha) {
-                return res.status(400).send({ error:  "Token inválido" } )
-            }
-            const horaAtual = new Date();
-            if (horaAtual > user.tokenResetExpiresSenha) {
-                return res.status(400).send({ error:  "Token expirado, gere outro" } )
-            }
-          
-            schema
-                .is().min(8)                                    // Minimum length 8
-                .is().max(100)                                  // Maximum length 100
-                .has().uppercase([1])                           // Must have uppercase letters
-                .has().lowercase()                              // Must have lowercase letters
-                .has().digits(2)                                // Must have at least 2 digits
-                .has().symbols([1])
-                .has().not().spaces()                           // Should not have spaces
-                .is().not().oneOf(['Passw0rd', 'Password123', '12345', 'senha']); // Blacklist these values
-            if (!schema.validate(senha)) {
-                return res
-                    .status(400)
-                    .send({ error: 'Senha Fraca, deve ter no mínimo 8 caracteres tem que ter no mínimo 1 letra Maiuscula Dois Digitos Sem espaço' } )
-            }
-            user.senha = senha;
-            await user.save();
-            return res.status(200).send({ message: "Atualizado com sucesso" })
-        } catch (error) {
-            console.log(error);
-            return res.status(400).send({ error: "Erro em resetar a senha, tente novamente" })
-        }
-    },
-}
+  async store(req, res) {
+    const { tokenResetSenha, senha, email } = req.body;
+    try {
+      const user = await User.findOne({ email }).select('+tokenResetSenha tokenResetExpiresSenha');
+      if (!user) {
+        return res.status(400).send({ error: 'Usuário Não encontrado' });
+      }
+      if (tokenResetSenha !== user.tokenResetSenha) {
+        return res.status(400).send({ error: 'Token inválido' });
+      }
+      const horaAtual = new Date();
+      if (horaAtual > user.tokenResetExpiresSenha) {
+        return res.status(400).send({ error: 'Token expirado, gere outro' });
+      }
+
+      schema
+        .is().min(8) // Minimum length 8
+        .is().max(100) // Maximum length 100
+        .has()
+        .uppercase([1]) // Must have uppercase letters
+        .has()
+        .lowercase() // Must have lowercase letters
+        .has()
+        .digits(2) // Must have at least 2 digits
+        .has()
+        .symbols([1])
+        .has()
+        .not()
+        .spaces() // Should not have spaces
+        .is()
+        .not()
+        .oneOf(['Passw0rd', 'Password123', '12345', 'senha']); // Blacklist these values
+      if (!schema.validate(senha)) {
+        return res
+          .status(400)
+          .send({ error: 'Senha Fraca, deve ter no mínimo 8 caracteres tem que ter no mínimo 1 letra Maiuscula Dois Digitos Sem espaço' });
+      }
+      user.senha = senha;
+      await user.save();
+      return res.status(200).send({ message: 'Atualizado com sucesso' });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send({ error: 'Erro em resetar a senha, tente novamente' });
+    }
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,5 @@
 const express = require('express');
-const multer = require('multer');
-const uploadConfig = require('./config/upload');
-
 const routes = express.Router();
-const upload = multer(uploadConfig);
 
 const UsuarioController = require('./app/controllers/UsuarioController');
 const SessionController = require('./app/controllers/SessionController');
@@ -12,18 +8,20 @@ const ResetPasswordController = require('./app/controllers/ResetPasswordControll
 const AgendaController = require('./app/controllers/AgendaController');
 const auth = require('./app/middlewares/auth');
 
-routes.post('/user',UsuarioController.store);
-routes.put('/user',auth,upload.single('cpfImages'),UsuarioController.update);
-routes.get('/user',auth,UsuarioController.index);
-routes.get('/perfil',auth,UsuarioController.show);
+routes.post('/user', UsuarioController.store);
+routes.put('/user', auth, UsuarioController.update);
+routes.get('/user', auth, UsuarioController.index);
+routes.get('/user/page',auth,UsuarioController.page);
+routes.get('/doctor/page',auth,UsuarioController.listPageDoctor);
+routes.get('/perfil', auth, UsuarioController.show);
 
-routes.post('/sessions',SessionController.store);
-routes.post('/forgot-password',ForgotPasswordController.store);
-routes.post('/reset-password',ResetPasswordController.store);
+routes.post('/sessions', SessionController.store);
+routes.post('/forgot-password', ForgotPasswordController.store);
+routes.post('/reset-password', ResetPasswordController.store);
 
-routes.post('/agenda',auth,AgendaController.store);
-//routes.get('/agenda',auth,AgendaController.index);
-routes.get('/agendaProfissional',auth,AgendaController.agendaProfissional);
-routes.get('/agendaPaciente',auth,AgendaController.agendaPaciente);
+routes.post('/agenda', auth, AgendaController.store);
+// routes.get('/agenda',auth,AgendaController.index);
+routes.get('/agendaProfissional', auth, AgendaController.agendaProfissional);
+routes.get('/agendaPaciente', auth, AgendaController.agendaPaciente);
 
 module.exports = routes;
